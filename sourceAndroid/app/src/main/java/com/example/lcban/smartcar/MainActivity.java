bcac914650eaa825dd60b0382a2c5945bfedb366
lecongbang90 <lcbangtv90@gmail.com>
2018-07-28 21:02:59 +0700

    ADD

--- sourceAndroid/app/src/main/java/com/example/lcban/smartcar/MainActivity.java
+++ sourceAndroid/app/src/main/java/com/example/lcban/smartcar/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.lcban.smartcar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private Button bt;
+    private EditText ip;
+    private EditText port;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bt = (Button)findViewById(R.id.btConnect);
+        ip = (EditText)findViewById(R.id.etIp);
+        port = (EditText)findViewById(R.id.etPort);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(it);
+            }
+        });
     }
 }
